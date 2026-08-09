04e3f1ad1776844ff7f416eef544833b705c6668
boymeetsblockchain <ajeoluwasegun18@gmail.com>
2026-08-09 06:50:19 +0100

    feat: implement database connection and logging setup

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"github.com/boymeetsblockchain/e-commerce/internal/config"
+	"github.com/boymeetsblockchain/e-commerce/internal/database"
+	"github.com/boymeetsblockchain/e-commerce/internal/logger"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	log := logger.New()
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to load config")
+	}
+
+	db, err := database.New(cfg.Database)
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to connect to database")
+	}
+
+	mainDB, err := db.DB()
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to get database connection")
+	}
+
+	defer mainDB.Close()
+	gin.SetMode(cfg.Server.GinMode)
+
+	log.Info().Msg("starting server")
+
 }
