8c793786ffee5c29fd818e2be028800ad72c2d77
boymeetsblockchain <ajeoluwasegun18@gmail.com>
2026-08-10 04:33:03 +0100

    feat:added jwt utils

---
+++ utils/jwt.go
@@ -0,0 +1,65 @@
+package utils
+
+import (
+	"errors"
+	"time"
+
+	"github.com/boymeetsblockchain/e-commerce/internal/config"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type Claim struct {
+	UserID uint   `json:"user_id"`
+	Email  string `json:"email"`
+	Role   string `json:"role"`
+	jwt.RegisteredClaims
+}
+
+func generateJWT(cfg *config.JWTConfig, userId uint, email, role string) (accessToken string, refreshToken string, err error) {
+	accessClaims := &Claim{
+		UserID: userId,
+		Email:  email,
+		Role:   role,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Duration(cfg.ExpiresIn) * time.Second)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	at := jwt.NewWithClaims(jwt.SigningMethodHS256, accessClaims)
+	accessToken, err = at.SignedString([]byte(cfg.Secret))
+	if err != nil {
+		return "", "", err
+	}
+	refreshClaims := &Claim{
+		UserID: userId,
+		Email:  email,
+		Role:   role,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Duration(cfg.RefreshTokenExpires) * time.Second)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	rt := jwt.NewWithClaims(jwt.SigningMethodHS256, refreshClaims)
+	refreshToken, err = rt.SignedString([]byte(cfg.Secret))
+	if err != nil {
+		return "", "", err
+	}
+
+	return accessToken, refreshToken, nil
+}
+
+func validateToken(tokenString, secret string) (*Claim, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &Claim{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(secret), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	if claims, ok := token.Claims.(*Claim); ok && token.Valid {
+		return claims, nil
+	}
+
+	return nil, errors.New("invalid token")
+}
